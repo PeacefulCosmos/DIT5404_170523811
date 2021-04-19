bd14e6ae63444d13f561212408541f5f10948a41
roy chan <roy.chan@inreality.com>
2021-04-19 16:10:44 +0800

    add cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "express";
 import { route } from "./route.js";
+import cors from "cors";
 import { mongoConnection } from "./mongoose.js";
 
 const start = async () => {
   const PORT = 8080;
   const app = express();
 
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
